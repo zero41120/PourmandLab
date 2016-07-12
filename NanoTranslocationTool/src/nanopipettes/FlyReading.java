2096aa416b61df1058cd41cdd5afe295403696b7
Eric Lin <tlin34@ucsc.edu>
2016-07-12 16:11:07 -0700

    On fly reading from the machine. This probably will store data into a SQLite database. Need more research

---
+++ NanoTranslocationTool/src/nanopipettes/FlyReading.java
@@ -0,0 +1,91 @@
+package nanopipettes;
+
+import java.util.ArrayList;
+
+public class FlyReading extends DataProvider {
+
+	@Override
+	public void scanData(Double startTime, Double endTime) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<TNData> getData() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void appendData(TNData toAdd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void appendData(ArrayList<TNData> toAdd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void appendData(Double time, Double current, Double voltage) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<TNData> getData(Double rangeStart, Double rangeEnd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void scanPeaks() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<TNPeakData> getPeakSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addPeak(TNPeakData toAdd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPeak(ArrayList<TNPeakData> toAdd) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPeak(Double startTime, Double endTime, Double average) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<TNPeakData> getPeakSet(Double rangeStart, Double rangeEnd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+
+}
