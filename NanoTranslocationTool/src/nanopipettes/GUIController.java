52e772222ad17839b4e028810a86e1e7bd1ffd38
Eric Lin <tlin34@ucsc.edu>
2016-07-12 16:47:19 -0700

    Basic GUI

--- NanoTranslocationTool/src/nanopipettes/GUIController.java
+++ NanoTranslocationTool/src/nanopipettes/GUIController.java
@@ -1,10 +1,43 @@
 package nanopipettes;
 
-public class GUIController {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class GUIController implements Initializable {
+	// @formatter:off
+	@FXML Button TNButtonLoadText;
+	// TODO add all fxml element
+	// @formatter:on
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub	
+	}
+	
+	public void TNActionClickLoadText(){
+		
+	}
+	
+	public void TNActionClickScanMachine(){
+		
+	}
+	
+	public void TNActionClickOutputExcel(){
+		
+	}
+	
+	public void TNActionClickOutputText(){
+		
+	}
+
+	// TODO add all fxml action
 
 }
